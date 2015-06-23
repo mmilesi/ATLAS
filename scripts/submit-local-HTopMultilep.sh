8969bafb6bf440b8b7b28d00abcc396acfcf026f
Marco Milesi <marco.milesi@cern.ch>
2015-06-23 08:19:21 +0000

    add some missing runs for period A4 in doc/list-grid-DxAOD-2015-13TeV.txt

--- scripts/submit-local-HTopMultilep.sh
+++ scripts/submit-local-HTopMultilep.sh
@@ -12,5 +12,5 @@
 #test-HTopMultilep --inDSType DxAOD-2015-13TeV --inDir /data/mmilesi/HTopMultileptonsTestSamples/HIGG8D1_20.1.5.1/ --inDSName mc15_13TeV.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.merge.DAOD_HIGG8D1.e3698_s2608_s2183_r6630_r6264_p2363_tid05629971_00 --outDir out_DxAOD_HTopMultilep_mc --maxEvents 10000
 #test-HTopMultilep --inDSType DxAOD-2015-13TeV --inDir /data/mmilesi/HTopMultileptonsTestSamples/HIGG8D1_20.1.5.1/ --inDSName mc15_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.merge.DAOD_HIGG8D1.e3601_s2576_s2132_r6630_r6264_p2363 --outDir out_DxAOD_HTopMultilep_mc --maxEvents 10000
 #test-HTopMultilep --inDSType DxAOD-2015-13TeV --inDir /data/mmilesi/HTopMultileptonsTestSamples/HIGG8D1_20.1.5.1_FullTruth/ --inDSName mc15_13TeV.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.merge.DAOD_HIGG8D1.e3698_s2608_s2183_r6630_r6264_p2363 --outDir out_DxAOD_HTopMultilep_mc --maxEvents -1
-#######################################
 test-HTopMultilep --inDSType DxAOD-2015-13TeV --inDir /data/mmilesi/HTopMultileptonsTestSamples/crash_Zee/ --inDSName mc15_13TeV --outDir out_DxAOD_HTopMultilep_mc --maxEvents 1000 
+#######################################
