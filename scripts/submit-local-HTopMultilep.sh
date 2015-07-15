3cbefbad6fc9606d1d8492d98caeb437eedfeeb4
Marco Milesi <marco.milesi@cern.ch>
2015-07-15 06:05:18 +0000

    Add some description in submit-local-HTopMultilep.sh

--- scripts/submit-local-HTopMultilep.sh
+++ scripts/submit-local-HTopMultilep.sh
@@ -1,9 +1,22 @@
 #######################################
 # full xAOD - mc15 13TeV  
 # ----------------------
-#test-HTopMultilep --inDSType DxAOD-2015-13TeV --inDir /data/mmilesi/HTopMultileptonsTestSamples/MC15/ --inDSName mc15_13TeV.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.merge.AOD.e3698_s2608_s2183_r6630_r6264 --outDir out_xAOD_HTopMultilep_mc --maxEvents -1
+#
+# ttbar non all had - 410000
+#
+test-HTopMultilep --inDSType DxAOD-2015-13TeV --inDir /data/mmilesi/HTopMultileptonsTestSamples/MC15/ --inDSName mc15_13TeV.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.merge.AOD.e3698_s2608_s2183_r6630_r6264 --outDir out_xAOD_HTopMultilep_mc --maxEvents -1
+#
+# Z-->mumu PowhegPythia inclusive - 361107
+#
 #test-HTopMultilep --inDSType DxAOD-2015-13TeV --inDir /data/mmilesi/HTopMultileptonsTestSamples/MC15/ --inDSName mc15_13TeV.361107.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zmumu.merge.AOD.e3601_s2576_s2132_r6630_r6264 --outDir out_xAOD_HTopMultilep_mc --maxEvents 1000
-test-HTopMultilep --inDSType DxAOD-2015-13TeV --inDir /data/mmilesi/HTopMultileptonsTestSamples/MC15/ --inDSName mc15_13TeV.361372.Sherpa_CT10_Zee_Pt0_70_CVetoBVeto.merge.AOD.e3651_s2586_s2174_r6793_r6264 --outDir out_xAOD_HTopMultilep_mc --maxEvents -1
+#
+# Z-->ee PowhegPythia inclusive - 361106
+#
+#test-HTopMultilep --inDSType DxAOD-2015-13TeV --inDir /data/mmilesi/HTopMultileptonsTestSamples/MC15/ --inDSName mc15_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.merge.AOD.e3601_s2576_s2132_r6630_r6264 --outDir out_xAOD_HTopMultilep_mc --maxEvents -1
+#
+# Z-->ee Sherpa 0 < pT(Z) < 70 GeV - 361372
+#
+#test-HTopMultilep --inDSType DxAOD-2015-13TeV --inDir /data/mmilesi/HTopMultileptonsTestSamples/MC15/ --inDSName mc15_13TeV.361372.Sherpa_CT10_Zee_Pt0_70_CVetoBVeto.merge.AOD.e3651_s2586_s2174_r6793_r6264 --outDir out_xAOD_HTopMultilep_mc --maxEvents -1
 #######################################
 # data15 13TeV DAOD - Week 1 
 # --------------------------
