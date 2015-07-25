5044880ba2bc32655d5448a225e7ef0ac06e02ef
Marco Milesi <marco.milesi@cern.ch>
2015-07-25 06:49:39 +0000

    Ready to launch a test grid production (test11)

--- scripts/submit-grid-HTopMultilep.sh
+++ scripts/submit-grid-HTopMultilep.sh
@@ -3,4 +3,4 @@
 #  -) data: 20.1.5.1
 #  -) mc:   20.1.5.1
 # ----------------------
-test-HTopMultilep --outDir outputtest_grid_HTopMultilep-DxAOD-2015-13TeV --grid 1 --gridUser mmilesi --inGridDSList HTopMultilepAnalysis/doc/list-grid-DxAOD-2015-13TeV.txt --inDSType DxAOD-2015-13TeV --outDSID HTopMultilep.010tris_DxAOD
+test-HTopMultilep --outDir outputtest_grid_HTopMultilep-DxAOD-2015-13TeV --grid 1 --gridUser mmilesi --inGridDSList HTopMultilepAnalysis/doc/list-grid-DxAOD-2015-13TeV.txt --inDSType DxAOD-2015-13TeV --outDSID HTopMultilep.test11_DxAOD
