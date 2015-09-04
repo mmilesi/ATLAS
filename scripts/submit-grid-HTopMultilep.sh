8984280726b5e4e654d22f0530604ae7e41fd56d
Marco Milesi <marco.milesi@cern.ch>
2015-09-04 07:17:02 +0000

    Change ExpectedRates.root to fix for incorrect pT binning for fake rates

--- scripts/submit-grid-HTopMultilep.sh
+++ scripts/submit-grid-HTopMultilep.sh
@@ -3,5 +3,5 @@
 #  -) data: 20.1.5.1
 #  -) mc:   20.1.5.1
 # ----------------------
-test-HTopMultilep --outDir outputtest_grid_HTopMultilep-DxAOD-2015-13TeV --grid 1 --gridUser mmilesi --inGridDSList HTopMultilepAnalysis/doc/list-grid-DxAOD-2015-13TeV.txt --inDSType DxAOD-2015-13TeV --outDSID HTopMultilep.013B_Run2OR_TruthTP_DxAOD
+test-HTopMultilep --outDir outputtest_grid_HTopMultilep-DxAOD-2015-13TeV --grid 1 --gridUser mmilesi --inGridDSList HTopMultilepAnalysis/doc/list-grid-DxAOD-2015-13TeV.txt --inDSType DxAOD-2015-13TeV --outDSID HTopMultilep.013D_DxAOD
 
