67d2dfcf8ae4258afe014e7a068beedb48f36998
Marco Milesi <marco.milesi@cern.ch>
2015-11-18 12:13:10 +0000

    Ready to tag 00-01-16

--- scripts/submit-grid-HTopMultilep.sh
+++ scripts/submit-grid-HTopMultilep.sh
@@ -1,2 +1,2 @@
-test-HTopMultilep --outDir outputtest_grid_HTopMultilep-DxAOD-2015-13TeV --grid 1 --gridUser mmilesi --inGridDSList HTopMultilepAnalysis/doc/list-grid-DxAOD-2015-13TeV.txt --inDSType DxAOD-2015-13TeV --outDSID HTopMultilep.020b_DxAOD
+test-HTopMultilep --outDir outputtest_grid_HTopMultilep-DxAOD-2015-13TeV --grid 1 --gridUser mmilesi --inGridDSList HTopMultilepAnalysis/doc/list-grid-DxAOD-2015-13TeV.txt --inDSType DxAOD-2015-13TeV --outDSID HTopMultilep.021b_DxAOD
 
