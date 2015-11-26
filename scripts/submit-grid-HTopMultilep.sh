4ac8d3cea02d99e69cdaf103a26f865aa9385334
Marco Milesi <marco.milesi@cern.ch>
2015-11-26 07:02:10 +0000

    Add --inputDQ2 to xAH_run.py grid submission command

--- scripts/submit-grid-HTopMultilep.sh
+++ scripts/submit-grid-HTopMultilep.sh
@@ -2,7 +2,7 @@
 
 username=mmilesi
 
-prodtag=021_testLeak1_DxAOD
+prodtag=021_testLeak2_DxAOD
 
 infilepath=HTopMultilepAnalysis/doc/list-grid-DxAOD-2015-13TeV.txt
 
@@ -12,9 +12,9 @@ current_time="$(date +'%d-%m-%Y-%T')"
 outdir=output_grid_DxAOD-2015-13TeV_${current_time}
 
 destSE=AUSTRALIA-ATLAS_LOCALGROUPDISK
-
-#exclSE=
+exclSE=ANALY_IHEP
 
 gridDSname=user.${username}.HTopMultilep.${prodtag}.%in:name[2]%.%in:name[3]%
 
-xAH_run.py --files ${infilepath} --config ${configpath} --inputList --submitDir ${outdir} prun --optGridMergeOutput=1 --optGridNFilesPerJob=1.0 --optGridDestSE=${destSE} --optGridOutputSampleName=${gridDSname} #--optGridExcludedSite=${exclSE}
+xAH_run.py -vv --files ${infilepath} --config ${configpath} --inputList --inputDQ2 --submitDir ${outdir} prun --optGridMergeOutput=1 --optGridNFilesPerJob=1.0 --optGridDestSE=${destSE} --optGridOutputSampleName=${gridDSname} --optGridExcludedSite=${exclSE}
+
