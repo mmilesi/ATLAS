d131333b8b7db4439bfc2beb0a297f25b8affdd1
Marco Milesi <marco.milesi@unimelb.edu.au>
2018-06-13 02:22:32 +0000

    Add a function to plot electron fake rate as a function of NBJets only.

--- PlotUtils/Plotter/EfficiencyPlotterScripts/EfficiencyPlotter_QualityPlots.py
+++ PlotUtils/Plotter/EfficiencyPlotterScripts/EfficiencyPlotter_QualityPlots.py
@@ -8,15 +8,15 @@ from ROOT import TLine
 from EfficiencyPlotterClasses import Plot, MultiPlot
 
 luminosity = 36.1
-ATLASlabel = "Internal"
-#ATLASlabel = ""
+#ATLASlabel = "Internal"
+ATLASlabel = ""
 
-# eff_filepath = "./PLOTS_25ns_v29/OutputPlots_MMRates_25ns_v29_TopAsymmConv/LeptonEfficiencies.root"
-eff_filepath = "./PLOTS_25ns_v29/OutputPlots_MMRates_25ns_v29_TopAsymmConv/LeptonEfficiencies_15GeV.root"
+eff_filepath = "./PLOTS_25ns_v29/OutputPlots_MMRates_25ns_v29_TopAsymmConv/LeptonEfficiencies.root"
+# eff_filepath = "./PLOTS_25ns_v29/OutputPlots_MMRates_25ns_v29_TopAsymmConv/LeptonEfficiencies_15GeV.root"
 # eff_filepath = "./PLOTS_25ns_v29/OutputPlots_MMClosureRates_25ns_v29/LeptonEfficiencies.root"
 
-# outdirbase =  "./PLOTS_25ns_v29/OutputPlots_MMRates_25ns_v29_TopAsymmConv/EfficiencyPlots/QualityPlots"
-outdirbase =  "./PLOTS_25ns_v29/OutputPlots_MMRates_25ns_v29_TopAsymmConv/EfficiencyPlots/QualityPlots_15Gev"
+outdirbase =  "./PLOTS_25ns_v29/OutputPlots_MMRates_25ns_v29_TopAsymmConv/EfficiencyPlots/QualityPlots"
+# outdirbase =  "./PLOTS_25ns_v29/OutputPlots_MMRates_25ns_v29_TopAsymmConv/EfficiencyPlots/QualityPlots_15Gev"
 # outdirbase =  "./PLOTS_25ns_v29/OutputPlots_MMClosureRates_25ns_v29/EfficiencyPlots/QualityPlots"
 
 def plotRealEfficiency():
@@ -135,6 +135,48 @@ def plotFakeEfficiency_El():
         os.makedirs(outdir)
 
     multiP.makeMultiPlot( outdir, "FakeRate_El" )
+
+    # Clear before returning
+
+    del plotlist[:]
+
+
+def plotFakeEfficiency_El_NBJetsOnly():
+
+    plotlist = []
+
+    p0_props = {
+                "yAxisTitle"  : "#varepsilon",
+                "yAxisRange"  : (0.0,0.2),
+                "xAxisTitle"  : "N_{#it{b}-tags}",
+                #"xAxisLog"    : True,
+                #"xAxisNdivisions" : 1,
+                "colour"      : kBlue,
+                "lineStyle"   : 2,
+                "lineWidth"   : 3,
+                "markerStyle" : 24,
+                "markerSize"  : 1.4,
+               }
+
+    p0 = Plot("RESCALED_2L_ee_Fake_El_NBJets_VS_Pt_Efficiency_observed_sub_projNBJets_1",eff_filepath, p0_props)
+    p0.addSystematics(syslist=["ND_VV","ND_TTV","ND_OtherPromptSS","N_QMisID","D_QMisID","ND_ALPHA"])
+
+    plotlist.append(p0)
+
+    multiP = MultiPlot( plots=plotlist )
+    multiP.luminosity = luminosity
+    # multiP.buildLegend( header="#bf{Data - #mu^{#pm}e^{#pm}}",lcoords=[0.65,0.7,0.93,0.9] ) # Unbolded
+    multiP.buildLegend( header="#bf{Data - #mu^{#pm}e^{#pm}}",lcoords=[0.64,0.72,0.92,0.928] ) # Unbolded
+
+    multiP.ATLASlabel = ATLASlabel
+    multiP.setCanvasCoords([50,50,800,800])
+    # multiP.setPlotTitle("Electron fake rate",(0.2,0.7))
+
+    outdir = outdirbase + ATLASlabel
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    multiP.makeMultiPlot( outdir, "FakeRate_El_NBJetsOnly" )
 
     # Clear before returning
 
